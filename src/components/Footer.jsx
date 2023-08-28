b00b71735f26044a710577cbdca6f4972d70b570
Mohd Hashim Saifi <hashimsaifi7480@gmail.com>
2023-08-28 20:38:50 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Techy_Hashim</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/meabhisingh" target={"blank"}>
+            Github
+          </a>
+          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
